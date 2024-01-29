d5a0738c74196a0e195ee85fcbcc4a48f64ab8f2
goda <goda@ubuntu.myguest.virtualbox.org>
2024-01-29 22:21:30 +0200

    Updated

--- src/kernel.c
+++ src/kernel.c
@@ -5,9 +5,7 @@ void kmain(void);
 
 void kmain(void){
     initGdt();
-    /* Initialize terminal interface */
 	terminal_initialize();
  
-	/* Newline support is left as an exercise. */
 	terminal_writestring("I can't believe that i did it ... it works ..... finally it works ... my back hurts so much ... maybe i should take a break ... yeah yeah a break ... that would be great ... ahhhh.");
 }
